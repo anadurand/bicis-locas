04acdf8a93124d2270e597c866361107c4079ee4
Ana Durand <ana.dc1004@gmail.com>
2017-03-02 13:25:08 -0500

    casos validados html y java

--- js/main.js
+++ js/main.js
@@ -1,3 +1,13 @@
 function validateForm(){
 	/* Escribe tú código aquí */
+  var password = document.getElementById("input-password").value;
+  var bici = document.getElementById("opciones").value;
+
+  if(password == "password" || password == "123456" || password == "098754"){
+    alert("Valor de password invalido, por favor ingresar nuevo");
+  }
+  if(bici == "0"){
+    alert("Por favor Ingresar valor de bici");
+  }
+
 }
