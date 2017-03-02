59993902168058d14b1f4f8cda602763f097194c
Ana Durand <ana.dc1004@gmail.com>
2017-03-02 16:08:49 -0500

    avance validacion js

--- js/main.js
+++ js/main.js
@@ -1,3 +1,23 @@
 function validateForm(){
 	/* Escribe tú código aquí */
+ var name = document.getElementById("name").value;
+ var apell = document.getElementById("lastName")
+ var password = document.getElementById("input-password").value;
+ var bici = document.getElementById("opciones").value;
+if((name != "" && apell  != "") && (password != "" && bici != "")){
+  if(name.match(/[A-Z]{1,}[a-z]+/) == null){
+    alert("nien");
+  }
+  if(password == "password" || password == "123456" || password == "098754"){
+    alert("Valor de password invalido, por favor ingresar nuevo");
+  }
+  if(bici == "0"){
+    alert("Por favor Ingresar valor de bici");
+  }
+
+
+}else{
+  alert("Falta Ingresar Valores");
+}
+
 }
