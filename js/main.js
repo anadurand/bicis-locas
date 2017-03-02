e5d6015115e0fd66ab48e275865c6660b7d5ef87
Ana Durand <ana.dc1004@gmail.com>
2017-03-02 17:53:10 -0500

    validacion final js

--- js/main.js
+++ js/main.js
@@ -1,15 +1,24 @@
 function validateForm(){
 	/* Escribe tú código aquí */
  var name = document.getElementById("name").value;
- var apell = document.getElementById("lastName")
+ var apell = document.getElementById("lastname").value;
  var password = document.getElementById("input-password").value;
+ var mail = document.getElementById("input-email").value;
  var bici = document.getElementById("opciones").value;
+
 if((name != "" && apell  != "") && (password != "" && bici != "")){
-  if(name.match(/[A-Z]{1,}[a-z]+/) == null){
-    alert("nien");
+  if(!name.match(/[A-Z]{1,}[a-z]+/)){
+    alert("Error al ingresar Nombre: \n Ingresar primera letra máyuscula");
   }
-  if(password == "password" || password == "123456" || password == "098754"){
-    alert("Valor de password invalido, por favor ingresar nuevo");
+  if(!apell.match(/[A-Z]{1,}[a-z]+/)){
+    alert("Error al ingresar Apellido: \n Ingresar primera letra máyuscula");
+  }
+  if(!mail.match(/[a-z0-9._%+-]+@[a-z0-9.-]+\.com$/)){
+    alert("Error al ingresar Email: \n Ingresar email@domain.com");
+  }
+
+  if(password.length != 6 || password == "password" || password == "123456" || password == "098754"){
+    alert("Valor de password invalido, \n Ingresar password de 6 caracteres");
   }
   if(bici == "0"){
     alert("Por favor Ingresar valor de bici");
