9d71f08b34454bb832e42b69e27db82270b2cb7b
Ana Durand <ana.dc1004@gmail.com>
2017-03-02 15:49:24 -0500

    validacion con html y js

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@ function validateForm(){
 	/* Escribe tú código aquí */
   var password = document.getElementById("input-password").value;
   var bici = document.getElementById("opciones").value;
-
+  
   if(password == "password" || password == "123456" || password == "098754"){
     alert("Valor de password invalido, por favor ingresar nuevo");
   }
